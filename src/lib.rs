8a1c0def7b26a7c1707812af9e94c3878b59e6a8
Pierre Avital <pierre.avital@me.com>
2023-03-29 11:37:50 +0200

    make std feature additive

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 //! This allows a more convenient usage, where cells and tokens can be constructed independently, with the same compile-time guarantees as [`ghost-cell`](https://crates.io/crates/ghost-cell). The trade-off for this arguably more convenient usage and arguably easier to understand branding method is that tokens, while zero-sized if made correctly, must be guaranteed to be constructable only if no other instance exists.
 //!
 //! To this end, this crate provides the [`generate_token`] macro, which will create a ZST which can only be constructed using [`TokenTrait::aquire`], which is generated to guarantee no other token exists before returning the token. This is done by checking a static `AtomicBool` flag, which is the only runtime cost of these tokens.
-#![no_std]
+#![cfg_attr(not(features = "std"), no_std)]
 pub use paste::paste;
-#[cfg(not(features = "no_std"))]
+#[cfg(features = "std")]
 mod std {
     use crate::macros::{IdMismatch, SingletonUnavailable};
     extern crate std;
